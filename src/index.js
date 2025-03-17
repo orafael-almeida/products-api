e63325ce10fcf7024d6b512a941b88aa6638f9c5
Rafael Almeida <almeidarp.dev@gmail.com>
2025-03-16 21:22:50 -0300

    feat:  create POST endpoint for product creation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const productsRepository = [{ id: 1, productName: "Warwick" }];
+const productsRepository = [{ id: 1, productName: "Warwick", quantity: 5 }];
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -13,6 +13,18 @@ app.get("/products", function (req, res) {
   return res.json(productsRepository);
 });
 
+app.post("/products", function (req, res) {
+  const { productName, quantity } = req.body;
+  const product = {
+    id: 1,
+    productName,
+    quantity,
+  };
+
+  productsRepository.push(productName);
+
+  return res.status(201).json(product);
+});
 app.listen(port, () => {
   console.log(`✅ API Running on port ${port}`);
 });
